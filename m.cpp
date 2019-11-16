8e17713f433cd402d85c50c7590b8d3664b42158
alexinntekt <alex.inntekt@gmail.com>
2019-11-16 17:40:37 +0200

    sp

--- m.cpp
+++ m.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <cstring>
+
 using namespace std;
+
+int count = 1;
 
 int is_double(char *c)
 {
@@ -27,6 +32,35 @@ int is_double(char *c)
 	return 0;
 }
 
+
+void proc(char *str, int index)
+{
+	for(int i=index; i<(strlen(str)-1);i++)
+	{
+		char dbl [2] = {str[i] , str[i+1] };
+
+		if(is_double(dbl)==1)
+		{
+			count++;
+			proc(str,i+2);
+		}
+	}
+
+	// for i in range(index,len(str)-1):
+	// 	dbl=str[i]+str[i+1]
+	// 	# print(dbl)
+	// 	if(is_double(dbl)):
+	// 		start = str[i+2:len(str)]
+	// 		print(start+' - '+dbl )
+
+	// 		count+=1
+	// 		t*=2
+	// 		proc(str,i+2)
+}
+	
+
+
+
 void run()
 {
 
@@ -34,8 +68,11 @@ void run()
 
 	cin >> input;
 
+	proc(input,0);
 
-	cout << is_double(input);
+	cout << count;
+
+	// cout << is_double(input);
 }
 
 int main()
